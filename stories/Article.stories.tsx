7c3f7e735768d7a6c3b50c3e4dc30d598c30da7a
Jan Basoc <jbasoc@stratpoint.com>
2023-04-13 05:29:27 +0000

    chore: migrate stories file to stories folder

--- stories/Article.stories.tsx
+++ stories/Article.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 
-import PostArticle from './[slug]';
+import PostArticle from '../pages/post/[slug]';
 
 export default {
   title: 'Pages/Article',
--- stories/Home.stories.tsx
+++ stories/Home.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 
-import Home from './index';
+import Home from '../pages/index';
 
 export default {
   title: 'Pages/Home',
